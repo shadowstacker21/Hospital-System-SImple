761f01cad30fbb938648caeab26ebe6c1a3aa6d9
Alamin Hossain <ytakashahmed@gmail.com>
2025-11-18 19:57:18 +0600

    Frontend Domain ADD on cors-header

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -64,10 +64,8 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    # "https://example.com",
-    # "https://sub.example.com",
-    # "http://localhost:8080",
-    # "http://127.0.0.1:9000",
+    "http://localhost:5173",
+    
 ]
 
 ROOT_URLCONF = 'hospital_management.urls'
